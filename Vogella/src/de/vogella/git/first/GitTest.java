1b494eb1611bdc1031572af85e56078d0a951ab7
cheewei <yeapcw@outlook.com>
2015-02-07 00:28:33 +0800

    done cool4

--- Vogella/src/de/vogella/git/first/GitTest.java
+++ Vogella/src/de/vogella/git/first/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
 		System.out.println("Git is cool");
         System.out.println("Git is cool2");
         System.out.println("Git is cool3");
+        System.out.println("Git is cool4")
 	}
 }
